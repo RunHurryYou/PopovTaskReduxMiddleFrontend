58e5f388563ac574ea7089982dacdbb90caa4fb9
Yaroslav Popov <y.popov@globexit.ru>
2026-02-04 21:05:59 +0300

    feat(FilterForm): fix FilterForm

--- src/pages/ContactListPage.tsx
+++ src/pages/ContactListPage.tsx
@@ -1,50 +1,43 @@
 import { useState } from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 import { ContactCard } from 'src/components/ContactCard';
-//import { FilterFormValues } from 'src/components/FilterForm';
-//import { ContactDto } from 'src/types/dto/ContactDto';
+import { FilterForm, FilterFormValues } from 'src/components/FilterForm';
 import { ModalAddContact } from 'src/components/ModalAddContact';
 import { useAppSelector } from 'src/store/hooks';
+import { ContactDto } from 'src/types/dto/ContactDto';
 
 export const ContactListPage = () => {
   const [showModal, setShowModal] = useState(false);
   const contacts = useAppSelector((state) => state.contacts);
+  const groupContactsState = useAppSelector((state) => state.groupContacts);
+  const [findContacts, setFindContacts] = useState<ContactDto[]>(contacts.ids.map((id) => contacts.entities[id]));
 
-  // const onSubmit = (fv: Partial<FilterFormValues>) => {
-  //   let findContacts: ContactDto[] = contacts.ids.map((id) => contacts.entities[id]);
 
-  //   if (fv.name) {
-  //     const fvName = fv.name.toLowerCase();
-  //     findContacts = findContacts.filter(({ name }) => (
-  //       name.toLowerCase().indexOf(fvName) > -1
-  //     ))
-  //   }
+  const onSubmit = (fv: Partial<FilterFormValues>) => {
 
-  //   if (fv.groupId) {
-  //     const groupContacts = groupContactsState[0].find(({ id }) => id === fv.groupId);
+    if (fv.name) {
+      const fvName = fv.name.toLowerCase();
+      setFindContacts(Object.values(contacts.entities).filter((contact) => contact.name.toLowerCase().includes(fvName)));
+    }
 
-  //     if (groupContacts) {
-  //       findContacts = findContacts.filter(({ id }) => (
-  //         groupContacts.contactIds.includes(id)
-  //       ))
-  //     }
-  //   }
-
-  //   console.log(findContacts);
-  // }
+    if (fv.groupId) {
+      const groupId = fv.groupId;
+      setFindContacts((prev)=>prev.filter((contact) => groupContactsState.entities[groupId].contactIds.includes(contact.id)));
+    }
+  }
 
   return (
     <>
       <Row xxl={1}>
         <Col className="mb-3">
-          {/* <FilterForm groupContactsList={groupContactsState[0]} initialValues={{}} onSubmit={onSubmit} /> */}
+          <FilterForm initialValues={{}} onSubmit={onSubmit} />
           <Button onClick={() => setShowModal(true)}>Добавить</Button>
         </Col>
         <Col>
           <Row xxl={4} className="g-4">
-            {contacts.ids.map((id) => (
-              <Col key={id}>
-                <ContactCard contact={contacts.entities[id]} withLink />
+            {findContacts.map((elem) => (
+              <Col key={elem.id}>
+                <ContactCard contact={elem} withLink />
               </Col>
             ))}
           </Row>
