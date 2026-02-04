3c0811ce840af18078bea9974e2c18965bd81b25
Yaroslav Popov <y.popov@globexit.ru>
2026-02-04 21:35:48 +0300

    fix(ContactListPage): fix error with form

--- src/pages/ContactListPage.tsx
+++ src/pages/ContactListPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 import { ContactCard } from 'src/components/ContactCard';
 import { FilterForm, FilterFormValues } from 'src/components/FilterForm';
@@ -12,18 +12,26 @@ export const ContactListPage = () => {
   const groupContactsState = useAppSelector((state) => state.groupContacts);
   const [findContacts, setFindContacts] = useState<ContactDto[]>(contacts.ids.map((id) => contacts.entities[id]));
 
+  useEffect(() => {
+    setFindContacts(contacts.ids.map((id) => contacts.entities[id]));
+  }, [contacts]);
 
   const onSubmit = (fv: Partial<FilterFormValues>) => {
 
+    let newContacts = contacts.ids.map((id) => contacts.entities[id]);
+
     if (fv.name) {
       const fvName = fv.name.toLowerCase();
-      setFindContacts(Object.values(contacts.entities).filter((contact) => contact.name.toLowerCase().includes(fvName)));
+      newContacts = newContacts.filter((contact) => contact.name.toLowerCase().includes(fvName));
     }
 
     if (fv.groupId) {
       const groupId = fv.groupId;
-      setFindContacts((prev)=>prev.filter((contact) => groupContactsState.entities[groupId].contactIds.includes(contact.id)));
+      if(groupId !== "Open this select menu")
+        newContacts = newContacts.filter((contact) => groupContactsState.entities[groupId].contactIds.includes(contact.id));
     }
+
+    setFindContacts(newContacts);
   }
 
   return (
