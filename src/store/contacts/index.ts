ddf415125b5bf242fd169a34fc228a0915027bfb
Yaroslav Popov <y.popov@globexit.ru>
2026-02-10 09:53:27 +0300

    feat(Redux): delete Redux

--- src/store/contacts/index.ts
+++
@@ -1,6 +0,0 @@
-import { contactsApiSlice } from "./contacts.slice";
-
-export const contactsReducer = contactsApiSlice.reducer
-export const contactsReducerPath = contactsApiSlice.reducerPath
-export const contactsMiddleware = contactsApiSlice.middleware
-export const {useGetContactsQuery, useAddContactMutation, useDeleteContactMutation, useSwitchFavoriteContactMutation} = contactsApiSlice;
--- src/store/groupContacts/index.ts
+++
@@ -1,7 +0,0 @@
-import { groupContactsSlice } from "./groupContacts.slice";
-
-export const groupContactsReducer = groupContactsSlice.reducer
-export const groupContactsMiddleware = groupContactsSlice.middleware
-export const groupContactsReducerPath = groupContactsSlice.reducerPath
-
-export const {useGetGroupContactsQuery, useAddGroupContactMutation, useDeleteGroupContactMutation, useDeleteContactFromGroupMutation} = groupContactsSlice
